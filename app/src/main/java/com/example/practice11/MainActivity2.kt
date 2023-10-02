3da92d8b86b6fa3d7816187574ca29a82a886bd6
ezh01 <ezhtop@gmail.com>
2023-10-02 20:06:53 +0300

    first commit

---
+++ app/src/main/java/com/example/practice11/MainActivity2.kt
@@ -0,0 +1,26 @@
+package com.example.practice11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+    var status:Boolean = false
+    fun btnImage(view: View){
+        val btn: ImageButton = view as ImageButton
+        if (status == false) {
+            btn.setImageResource(R.drawable.btn_check_buttonless_on)
+            status = true
+        }
+        else if (status == true){
+            btn.setImageResource(R.drawable.btn_check_buttonless_off)
+            status = false
+        }
+    }
+}
